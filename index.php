e6dd4a9980a35a7488b0074fe7937de679fcc552
Angel Guillen <angel1631@gmail.com>
2013-09-07 09:02:15 -0600

    pagina index, para la administracion
    Esta es la pagina principal del control de evaluacion, es el index de la
    administracion. Para funcionar necesita granlibreria en la carpeta
    superior e importar las librerias de tecnodisa.com

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require_once("../granlibreria.php");
+	encabezado("Inicio de control de evaluacion, parte administracion");
+?>
+	<script type="text/javascript" src="http://tecnodisa.com/librerias/funciones.js"></script>
+	<label>Nombre Curso: </label><br>
+		<input type="text" class=".vacio solo_letras" id="nombre" />
+	<label>Inicia: </label>
+		<input type="text" class="fecha" id="inicia" />
+	<label>Finaliza: </label>
+		<input type="text" class="fecha" id="finaliza" />
+
+	
